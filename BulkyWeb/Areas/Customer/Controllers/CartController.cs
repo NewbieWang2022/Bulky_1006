dcd974ca4a0372b7976d7b3500a1de5d31081587
Daniel <wangyutang198@gmail.com>
2025-10-06 22:45:31 +0800

    bug
    includeProperties bug

---
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,31 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using BulkyBook.Models;
+
+namespace BulkyBookWeb.Areas.Customer.Controllers
+{
+    [Area("customer")]
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+
+            ShoppingCartVM = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
+                includeProperties: "Product")
+            };
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
